284e9e79acde60dd8def6e634d83d745949fca61
Eitan Chatav <eitan.chatav@gmail.com>
2018-01-11 13:28:44 -0800

    more equivalence code

--- rholang/src/main/scala/rholang/rosette/TypeChecker.scala
+++ rholang/src/main/scala/rholang/rosette/TypeChecker.scala
@@ -10,6 +10,9 @@ package coop.rchain.rho2rose
 
 // import coop.rchain.syntax.rholang._
 import coop.rchain.syntax.rholang.Absyn._
+import scalaz._
+import scalaz.std.list._
+import scalaz.std.option._
 
 object CheckerTypes {
     type TypeError = String
@@ -104,7 +107,7 @@ extends Chan.Visitor[Boolean,TPIndicator]{
 
 object Equivalences{
 
-  def nameEquivalent(n1: Chan, n2: Chan): Boolean = {
+  def nameEquivalent(env1: DeBruijn, n1: Chan, env2: DeBruijn, n2: Chan): Boolean = {
     (n1, n2) match {
       case (q1: CQuote, q2: CQuote) =>
         q1.proc_ match {
@@ -114,9 +117,13 @@ object Equivalences{
             case _ => structurallyEquivalent(q1.proc_, q2.proc_)
           }
         }
-      case _ => false // TODO: variable equality requires tracking variable environment
+      case (q1: CVar, q2: CVar) => env1.equivalent(q1.var_, env2, q2.var_)
+      case _ => false
     }
   }
+
+  def nameEquivalent(n1: Chan, n2: Chan): Boolean =
+    nameEquivalent(DeBruijn(), n1, DeBruijn(), n2)
 
   def structurallyNil(p: Proc): Boolean = {
     p match {
@@ -127,17 +134,85 @@ object Equivalences{
     }
   }
 
-  def structurallyEquivalent(p1: Proc, p2: Proc): Boolean = {
+  def structurallyEquivalent(env1: DeBruijn, p1: Proc, env2: DeBruijn, p2: Proc): Boolean = {
     (p1,p2) match {
       case (_ : PNil, _) => structurallyNil(p2)
       case (_, _ : PNil) => structurallyNil(p1)
+      // case (val1: PValue, val2: PValue) => valueEquivalent(env1,val1.value_,env2,val2.value_)
       // case () => {
       //   nameEquivalent(nsubj1, nsubj2)
       //   && structurallyNil()
       // }
-      case (_,_) => sys.error("unimplemented")
+      case (drop1: PDrop, drop2: PDrop) =>
+        nameEquivalent(env1, drop1.chan_, env2, drop2.chan_)
+      case (lift1: PLift, lift2: PLift) => {
+        nameEquivalent(env1, lift1.chan_, env2, lift2.chan_) &&
+          allStructurallyEquivalent(env1, lift1.listproc_, env2, lift2.listproc_)
+      }
+      case (print1: PPrint, print2: PPrint) =>
+        structurallyEquivalent(env1, print1.proc_, env2, print2.proc_)
+      case (constr1: PConstr, constr2: PConstr) => {
+        env1.equivalent(constr1.var_, env2, constr2.var_) &&
+          allStructurallyEquivalent(env1, constr1.listproc_, env2, constr2.listproc_)
+      }
+      case (contr1: PContr, contr2: PContr) => {
+        allCPatternEquivalent(env1, contr1.listcpattern_, env2, contr2.listcpattern_) match {
+          case None => false
+          case Some((newenv1,newenv2)) =>
+            env1.equivalent(contr1.var_, env2, contr2.var_) &&
+              structurallyEquivalent(newenv1, contr1.proc_, newenv2, contr2.proc_)
+        }
+      }
+        
+      case _ => false
     }
   }
+
+  def structurallyEquivalent(p1: Proc, p2: Proc): Boolean =
+    structurallyEquivalent(DeBruijn(), p1, DeBruijn(), p2)
+  
+  def allStructurallyEquivalent(env1: DeBruijn, ps1: ListProc, env2: DeBruijn, ps2: ListProc): Boolean = {
+    import scala.collection.JavaConverters._
+    ps1.size() == ps2.size() &&
+      (ps1.asScala.toList, ps2.asScala.toList).zipped.forall(
+        (proc1,proc2) => structurallyEquivalent(env1, proc2, env2, proc2)
+        )
+  }
+
+  def cpatternEquivalent(env1: DeBruijn, cp1: CPattern, env2: DeBruijn, cp2: CPattern): Option[(DeBruijn, DeBruijn)] = {
+    (cp1, cp2) match {
+      case (cpvar1: CPtVar, cpvar2: CPtVar) =>
+        (cpvar1.varpattern_, cpvar2.varpattern_) match {
+          case (_: VarPtWild, _: VarPtWild) => Some((env1,env2))
+          case (v1: VarPtVar, v2: VarPtVar) => {
+            Some((env1.newBindings(List(v1.var_)), env2.newBindings(List(v2.var_))))
+          }
+          case _ => None
+        }
+      case (cpval1: CValPtrn, cpval2: CValPtrn) => sys.error("unimplemented")
+      case (cpq1: CPtQuote, cpq2: CPtQuote) => sys.error("unimplemented")
+      case _ => None
+    }
+  }
+
+  def allCPatternEquivalent(env1: DeBruijn, cps1: ListCPattern, env2: DeBruijn, cps2: ListCPattern): Option[(DeBruijn, DeBruijn)] = {
+    import scala.collection.JavaConverters._
+    if (cps1.size() != cps2.size()) {
+      None
+    } else {
+      val list = cps1.asScala.toList.zip(cps2.asScala.toList)
+      def step(envs: (DeBruijn,DeBruijn), cps: (CPattern,CPattern)): Option[(DeBruijn,DeBruijn)] =
+        cpatternEquivalent(envs._1, cps._1, envs._2, cps._2)
+      Foldable[List].foldLeftM(list, (env1,env2)) (step _)
+    }
+  }
+  
+  // def valueEquivalent(env1: DeBruijn, v1: Value, env2: DeBruijn, v2: Value): Boolean = {
+  //   (v1,v2) match {
+  //     case (_: VQuant, _: VQuant) => quantityEquivalent(v1.)
+  //     case _ => false
+  //   }
+
 }
 
 class DeBruijn(val environment: Map[String,Int], val next: Int){
@@ -151,9 +226,21 @@ class DeBruijn(val environment: Map[String,Int], val next: Int){
     }
   }
 
+  def get(key: String): Option[Int] = environment.get(key)
+
+  def equivalent(key1: String, env: DeBruijn, key2: String) = {
+    (get(key1), env.get(key2)) match {
+      case (None,_) => false
+      case (_,None) => false
+      case (Some(ix1), Some(ix2)) => ix1 == ix2
+      }
+  }
+
 }
 
 object DeBruijn{
+
+  def apply(): DeBruijn = new DeBruijn()
 
   def apply(environment: Map[String,Int], next: Int): DeBruijn = {
     new DeBruijn(environment, next)
